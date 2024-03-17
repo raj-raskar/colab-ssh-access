dd96e25e7dff948e6c25ad0088f06e0d42ccdeb4
raj-raskar <134035279+raj-raskar@users.noreply.github.com>
2024-03-17 11:32:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-pip install openssh-server pwgen
 mkdir -p /var/run/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
-echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+echo -e "PermitRootLogin yes\nPasswordAuthentication yes" > /etc/ssh/sshd_config.d/00-rajvaibhav.conf
