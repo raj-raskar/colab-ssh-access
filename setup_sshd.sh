834dd7d2a66283f0bd26e9a2e9eda0f4a5ebf3f6
raj-raskar <134035279+raj-raskar@users.noreply.github.com>
2024-03-19 09:07:10 +0530

    Update setup_sshd.sh

--- setup_sshd.sh
+++ setup_sshd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-apt install openssh-server curl
+apt install openssh-server
 curl https://raw.githubusercontent.com/raj-raskar/colab-ssh-access/main/sshd_config | cat > /content/sshd_config
 /usr/bin/sshd -D -f /content/sshd_config
+passwd -d root
