b67e9df1e6eda24c91d3553f9737f720c3b11288
raj-raskar <134035279+raj-raskar@users.noreply.github.com>
2024-03-19 09:14:59 +0530

    Update and rename setup_sshd.sh to run_sshd.sh

--- setup_sshd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt install openssh-server
-curl https://raw.githubusercontent.com/raj-raskar/colab-ssh-access/main/sshd_config | cat > /content/sshd_config
-/usr/bin/sshd -D -f /content/sshd_config
-passwd -d root
